879cfd64ba749106f86057df607ae16990f784c4
Jan-Willem Buurlage <janwillembuurlage@gmail.com>
2018-04-23 14:16:18 +0200

    Actually implement fibonacci

--- src/fibonacci.cpp
+++ src/fibonacci.cpp
@@ -1,6 +1,17 @@
+#include <cassert>
 #include <vector>
+
 #include "fibonacci.hpp"
 
 std::vector<int> fibonacci(int n) {
-    return {1, 2, 3};
+    assert(n > 1);
+
+    auto xs = std::vector<int>(n);
+    xs[0] = 1;
+    xs[1] = 1;
+    for (int i = 2; i < n; ++i) {
+        xs[i] = xs[i - 1] + xs[i - 2];
+    }
+
+    return xs;
 }
